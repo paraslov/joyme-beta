39dc93c1bf989c1544460c7eca3cab46ab3482c6
Sergei Balanov <balanov@farzoom.com>
2023-08-01 08:00:35 +0600

    #36 (test): add auth by username bll tests

---
+++ src/features/AuthByUsername/model/selectors/getAuthState.test.ts
@@ -0,0 +1,34 @@
+import { DeepPartial } from '@reduxjs/toolkit'
+import { StateSchema } from 'app/providers/StoreProvider'
+import { getAuthState } from './getAuthState'
+
+describe('getAuthState test: ', () => {
+  test('returns authState', () => {
+    const state: DeepPartial<StateSchema> = { authForm: {
+      username: 'user',
+      password: 'pass',
+      errorMessage: 'error',
+      isLoading: true,
+    } }
+    const authState = getAuthState(state as StateSchema)
+
+    expect(authState).toStrictEqual({
+      username: 'user',
+      password: 'pass',
+      errorMessage: 'error',
+      isLoading: true,
+    })
+  })
+
+  test('returns default state', () => {
+    const state: DeepPartial<StateSchema> = {}
+    const authState = getAuthState(state as StateSchema)
+
+    expect(authState).toStrictEqual({
+      username: '',
+      password: '',
+      errorMessage: '',
+      isLoading: false
+    })
+  })
+})
