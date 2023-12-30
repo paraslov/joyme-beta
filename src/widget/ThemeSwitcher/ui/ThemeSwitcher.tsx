2a9320c4de79aac5993e978a01c4473b4361e019
Sergei Balanov <balanov@farzoom.com>
2023-12-30 09:05:18 +0600

    #44 (feat): fix loki tests

--- src/widget/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widget/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useMemo } from 'react'
+import React, { memo } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { Theme, useTheme } from 'app/providers/ThemeProvider'
 import LightIcon from 'shared/assets/icons/light.svg'
@@ -12,17 +12,14 @@ interface ThemeTogglerProps {
   className?: string
 }
 
+const themeIcon: Record<Theme, React.SVGProps<SVGSVGElement>> = {
+  [Theme.LIGHT]: <PinkIcon width={ 30 } height={ 30 } />,
+  [Theme.PINK]: <DarkIcon width={ 30 } height={ 30 } />,
+  [Theme.DARK]: <LightIcon width={ 30 } height={ 30 } />,
+}
+
 export const ThemeSwitcher = memo(({ className }: ThemeTogglerProps) => {
   const { theme, toggleTheme } = useTheme()
-
-  const themeIcon = useMemo(() => {
-    const themes = {
-      [Theme.DARK]: <LightIcon width={ 30 } height={ 30 } />,
-      [Theme.LIGHT]: <PinkIcon width={ 30 } height={ 30 } />,
-      [Theme.PINK]: <DarkIcon width={ 30 } height={ 30 } />,
-    }
-    return themes[theme]
-  }, [ theme ])
 
   return (
     <div className={ classNames(s.themeToggler, [ className ]) }>
@@ -30,7 +27,7 @@ export const ThemeSwitcher = memo(({ className }: ThemeTogglerProps) => {
         onClick={ toggleTheme }
         theme={ ButtonTheme.CLEAR }
       >
-        { themeIcon }
+        { themeIcon[theme] }
       </Button>
     </div>
   )
