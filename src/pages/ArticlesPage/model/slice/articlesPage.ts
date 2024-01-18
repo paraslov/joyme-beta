73294001ef4c6bfc1b49dca4621a1ed8b77cbd87
paraslov <paraslovjm@gmail.com>
2024-01-18 21:54:17 +0600

    #55 (feat): add articles page BLL

---
+++ src/pages/ArticlesPage/model/slice/articlesPage.ts
@@ -0,0 +1,52 @@
+import { createEntityAdapter, createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Article, ArticleListViewType } from 'entities/ArticleDetails'
+import { StateSchema } from 'app/providers/StoreProvider'
+import { ArticlesPageSchema } from '../types/articlesPageSchema'
+import { fetchArticlesList } from 'pages/ArticlesPage/model/services/fetchArticlesList'
+import { ARTICLE_PAGE_VIEW_TYPE } from 'shared/consts/localStorage'
+
+const articlesAdapter = createEntityAdapter<Article>({
+  selectId: (article) => article.id,
+})
+
+export const getArticles = articlesAdapter.getSelectors<StateSchema>(
+  (state) => state.articlesPage || articlesAdapter.getInitialState()
+)
+
+export const articlesPageSlice = createSlice({
+  name: 'articlesPage',
+  initialState: articlesAdapter.getInitialState<ArticlesPageSchema>({
+    view: ArticleListViewType.LIST,
+    ids: [],
+    entities: {},
+  }),
+  reducers: {
+    setView: (state, action: PayloadAction<ArticleListViewType>) => {
+      state.view = action.payload
+      localStorage.setItem(ARTICLE_PAGE_VIEW_TYPE, action.payload)
+    },
+    initState: (state) => {
+      state.view = localStorage.getItem(ARTICLE_PAGE_VIEW_TYPE) as ArticleListViewType
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchArticlesList.pending, (state) => {
+        state.isLoading = true
+        state.errorMessage = undefined
+      })
+      .addCase(fetchArticlesList.fulfilled, (state, action: PayloadAction<Article[]>) => {
+        state.isLoading = false
+        articlesAdapter.setAll(state, action.payload)
+      })
+      .addCase(fetchArticlesList.rejected, (state, action) => {
+        state.isLoading = false
+        state.errorMessage = action.payload
+      })
+  }
+})
+
+export const {
+  actions: articlesPageActions,
+  reducer: articlesPageReducer
+} = articlesPageSlice
--- src/pages/ArticlesPage/ui/ArticlePage.tsx
+++ src/pages/ArticlesPage/ui/ArticlePage.tsx
@@ -1,10 +1,22 @@
-import React, { memo } from 'react'
+import React, { memo, useCallback } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 
-import { ArticleList } from 'entities/ArticleDetails'
+import { ArticleList, ArticleListViewType } from 'entities/ArticleDetails'
+import { ReducersList, useDynamicModuleLoader } from 'shared/lib/DynamicModuleLoader/useDynamicModuleLoader'
+import { articlesPageActions, articlesPageReducer, getArticles } from '../model/slice/articlesPage'
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect'
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch'
+import { fetchArticlesList } from 'pages/ArticlesPage/model/services/fetchArticlesList'
+import { useSelector } from 'react-redux'
+import { getArticlesErrorMessage, getArticlesIsLoading, getArticlesView } from '../model/selectors/articlesPage'
+import { ArticlesPageViewSelector } from 'features/ArticlesPageViewSelector'
 
 interface ArticlePageProps {
   className?: string
+}
+
+const reducers: ReducersList = {
+  articlesPage: articlesPageReducer,
 }
 
 const ArticlePage: React.FC<ArticlePageProps> = (props: ArticlePageProps) => {
@@ -12,9 +24,26 @@ const ArticlePage: React.FC<ArticlePageProps> = (props: ArticlePageProps) => {
     className,
   } = props
 
+  const dispatch = useAppDispatch()
+  const articles = useSelector(getArticles.selectAll)
+  const isLoading = useSelector(getArticlesIsLoading)
+  const errorMessage = useSelector(getArticlesErrorMessage)
+  const viewType = useSelector(getArticlesView)
+
+  useDynamicModuleLoader({ reducers, removeAfterUnmount: true })
+  useInitialEffect(() => {
+    dispatch(fetchArticlesList())
+    dispatch(articlesPageActions.initState())
+  })
+
+  const onViewChange = useCallback((view: ArticleListViewType) => {
+    dispatch(articlesPageActions.setView(view))
+  }, [ dispatch ])
+
   return (
     <div className={ classNames('', [ className ]) }>
-      <ArticleList />
+      <ArticlesPageViewSelector view={ viewType } onViewChange={ onViewChange } />
+      <ArticleList articles={ articles } isLoading={ isLoading } viewType={ viewType } />
     </div>
   )
 }
