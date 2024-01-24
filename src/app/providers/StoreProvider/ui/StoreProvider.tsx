d767cd5b4d695bf8f6d335fcb082dea198a0292f
paraslov <paraslovjm@gmail.com>
2024-01-24 19:20:40 +0600

    #57 (fix): get rid of navigate in store, to prevent rerender

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux'
 import { createStore } from '../config/store'
 import { StateSchema } from '../config/StateSchema'
 import { ReducersMapObject } from '@reduxjs/toolkit'
-import { useNavigate } from 'react-router-dom'
 
 interface StoreProviderProps {
   children?: ReactNode
@@ -18,12 +17,9 @@ export const StoreProvider: React.FC<StoreProviderProps> = (props: StoreProvider
     asyncReducers,
   } = props
 
-  const navigate = useNavigate()
-
   const store = createStore(
     initialState as StateSchema,
     asyncReducers as ReducersMapObject<StateSchema>,
-    navigate,
   )
 
   return (
